e77b2f709afd71148d80762add1d6c689f0abce5
Kevin Costain <kevin@cwl.cc>
2020-11-30 17:52:18 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,20 @@
 
 set -e
 
-cd /var/www/html
+cd ~
 wget https://download.phpbb.com/pub/release/3.3/3.3.2/phpBB-3.3.2.zip
-bsdtar --strip-components=1 -xvf phpBB-3.3.2.zip
+bsdtar -xvf phpBB-3.3.2.zip
 rm phpBB-3.3.2.zip
+cd phpBB3
+
+if test -f "/var/www/html/config.php"; then
+    rm config.php
+fi
+mv  -v * /var/www/html
+
+cd /var/www/html
 chown -R www-data:www-data /var/www/html/
-touch config.php
+#touch config.php
 chown www-data:www-data config.php
 echo "core.disable_super_globals: false" >> /var/www/html/config/parameters.yml
 mkdir -p /var/phpbbdata
